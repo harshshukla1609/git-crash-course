0c33e36158f2c82135448515fd589af4178909ba
Harsh Vardhan Shukla <prfry9@gmail.com>
2024-04-18 23:31:48 +0530

    Revert changes

--- indes.js
+++ indes.js
@@ -1,14 +1,1 @@
 const age=24
-const name="Harry Potter"
-
-function sayMyName(){
-    console.log('My name is '+ name)
-}
-
-function isAdult(){
-    return age>=18
-}
-
-function newFn(){
-    console.log('This is new FN')
-}
